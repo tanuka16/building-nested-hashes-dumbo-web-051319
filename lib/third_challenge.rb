6a2fea86fe3a94a42b02916ecc1d88772c740c5e
Tanuka Das <das.tanuka@yahoo.com>
2019-05-02 05:43:43 +0000

    Automatically backed up by Learn

--- lib/third_challenge.rb
+++ lib/third_challenge.rb
@@ -8,6 +8,8 @@ def third_challenge
         },
       :matriarch => {
         #your key/value pairs here
+        :name => "Lody Montague"
+        :age => "53"
         },
       :hero => {
         #your key/value pairs here
