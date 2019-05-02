b0004fb60c1b86d9ca27bb5b6497b7b89f52429c
Tanuka Das <das.tanuka@yahoo.com>
2019-05-02 06:59:52 +0000

    Automatically backed up by Learn

--- lib/third_challenge.rb
+++ lib/third_challenge.rb
@@ -22,8 +22,7 @@ def third_challenge
         },
       :matriarch => {
         #your key/value pairs here
-        name: "Lody Capulet", age:"51"
-        },
+        name: "Lody Capulet", age:"51"},
       :heroine => { name: "Juliet", age: "15", status: "alive"},
       :heroine_friends => []                              #key points to an empty array
    }
