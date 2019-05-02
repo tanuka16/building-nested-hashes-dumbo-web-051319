6a882cd8051d1602d657576adf26fd9906561708
Tanuka Das <das.tanuka@yahoo.com>
2019-05-02 06:57:08 +0000

    Automatically backed up by Learn

--- lib/second_challenge.rb
+++ lib/second_challenge.rb
@@ -2,17 +2,17 @@ def second_challenge
   epic_tragedy = {
     :montague => {
       #your key/value pairs here!
-      :patriarch => {}                 #keys point to a value of an empty hash.
-      :matriarch => {}                 #keys point to a value of an empty hash.
-      :hero => {}                      #keys point to a value of an empty hash.
+      :patriarch => {},                 #keys point to a value of an empty hash.
+      :matriarch => {},                 #keys point to a value of an empty hash.
+      :hero => {},                      #keys point to a value of an empty hash.
       :hero_friends => []              # value of an empty array.
       },
     :capulet => {
       #your key/value pairs here!
-      :patriarch => {}
-      :matriarch => {}
-      :heroine => {}
-      :heroine_friends => [] 
+      :patriarch => {},
+      :matriarch => {},
+      :heroine => {},
+      :heroine_friends => []
       }}
 
 
